a17cc4daf98111aa22aed39837ced407e91f8c78
Allenyou <i@allenyou.wang>
2024-11-08 11:33:54 +0800

    20241108

--- src/components/LogicComponents/Matomo.tsx
+++ src/components/LogicComponents/Matomo.tsx
@@ -1,7 +1,11 @@
 "use client";
 
-import dynamic from "next/dynamic";
+import Script from "next/script";
 
-export const Matomo = dynamic(() => import("./MatomoInner"), {
-	ssr: false,
-});
+export function Matomo() {
+	return (
+		<Script strategy="lazyOnload">
+			{`const _paq = ((window as any)._paq = (window as any)._paq || []);_paq.push(["trackPageView"]);_paq.push(["enableLinkTracking"]);(function () {const u = "https://stat.allenyou.wang/";_paq.push(["setTrackerUrl", u + "matomo.php"]);_paq.push(["setSiteId", "4"]);const d = document,g = d.createElement("script"),s = d.getElementsByTagName("script")[0];g.async = true;g.src = u + "matomo.js";s.parentNode!.insertBefore(g, s);})();`}
+		</Script>
+	);
+}
