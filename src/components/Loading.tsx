10ce25d7c853d17b034234a935884186c4f865d6
Allenyou <i@allenyou.wang>
2025-06-30 21:04:07 +0800

    refactor: rewrite loading style

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,9 @@
-import "@/styles/utils.css";
+import loader from "@/styles/utils/loader.module.css";
 
 export function Loading() {
 	return (
-		<div className="loader-wrap">
-			<div className="loader" />
+		<div className={loader.wrap}>
+			<div className={loader.loader} />
 		</div>
 	);
 }
