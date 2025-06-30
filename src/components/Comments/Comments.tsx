30257aa368c31a1fa9c64ba7febb598986760feb
Allenyou <i@allenyou.wang>
2025-06-30 21:03:01 +0800

    refactor: rewrite comment style

--- src/components/Comments/Comments.tsx
+++ src/components/Comments/Comments.tsx
@@ -1,17 +1,20 @@
 "use client";
 import dynamic from "next/dynamic";
-import { Loading } from "./Loading";
 import { config } from "@/data/site-config";
 import React from "react";
 import "@/styles/comment.css";
+import { CommentsLoading } from "./CommentsLoading";
 
 const availableComments: Record<
 	SiteConfig["comment"]["type"],
 	() => Promise<React.FC>
 > = {
-	disable: async () => import("@/components/NoComments") as unknown as React.FC,
+	disable: async () =>
+		import("@/components/Comments/NoComments") as unknown as React.FC,
 	waline: async () =>
-		import("@/components/WalineComments") as unknown as React.FC,
+		import(
+			"@/components/Comments/WalineComments/WalineComments"
+		) as unknown as React.FC,
 };
 
 export const Comments = dynamic(availableComments[config.comment.type], {
@@ -20,14 +23,3 @@ export const Comments = dynamic(availableComments[config.comment.type], {
 		return <CommentsLoading />;
 	},
 });
-
-export function CommentsLoading() {
-	return (
-		<>
-			<div id="comment-loading">
-				<Loading />
-				<p id="comment-loading-text">加载评论中……</p>
-			</div>
-		</>
-	);
-}
