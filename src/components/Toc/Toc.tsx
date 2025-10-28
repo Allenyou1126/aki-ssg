cb90c31f39f794f7fcf047b1fa6b56dce706ce7b
Allenyou <i@allenyou.wang>
2025-10-28 20:58:04 +0800

    refactor: rewrite toc style

--- src/components/Toc/Toc.tsx
+++ src/components/Toc/Toc.tsx
@@ -5,12 +5,47 @@ import { scrollIntoViewById } from "@/utils/scrollIntoView";
 import type { Link, List, ListItem, Paragraph, Text } from "mdast";
 import { useEffect, useState, type ReactNode } from "react";
 import { createPortal } from "react-dom";
-import style from "./style.module.css";
+import * as stylex from "@stylexjs/stylex";
+
+const style = stylex.create({
+	toc: {
+		display: {
+			default: "none",
+			"@media (min-width: 1536px)": "block",
+		},
+		height: "100%",
+		left: "16px",
+		position: "absolute",
+	},
+	wrap: {
+		height: "85vh",
+		overflowY: "auto",
+		position: "sticky",
+		textWrap: "wrap",
+		top: "7rem",
+		width: "20rem",
+	},
+	placeholder: {
+		height: "10rem",
+	},
+	item: {
+		opacity: {
+			":hover": 0.8,
+		},
+		textAlign: "left",
+	},
+	list: {
+		fontWeight: 500,
+		opacity: 0.9,
+		paddingLeft: "2rem",
+		width: "100%",
+	},
+});
 
 function TocItem({ item }: { item: Paragraph }) {
 	return (
 		<button
-			className={style.item}
+			{...stylex.props(style.item)}
 			onClick={() => {
 				delay(10).then(() => {
 					scrollIntoViewById(
@@ -26,7 +61,7 @@ function TocItem({ item }: { item: Paragraph }) {
 function rendToc(toc: List | ListItem, index: number = 0): ReactNode {
 	if (toc.type === "list") {
 		return (
-			<ul key={index} className={style.list}>
+			<ul key={index} {...stylex.props(style.list)}>
 				{toc.children.map((it, index) => {
 					return rendToc(it, index);
 				})}
@@ -64,9 +99,9 @@ export default function Toc({ toc }: { toc: List }) {
 	const tocContent: ReactNode = rendToc(toc);
 	return mounted ? (
 		createPortal(
-			<div id="toc" className={style.toc}>
-				<div style={{ height: "10rem" }} />
-				<div className={style.wrap}>{tocContent}</div>
+			<div id="toc" {...stylex.props(style.toc)}>
+				<div {...stylex.props(style.placeholder)} />
+				<div {...stylex.props(style.wrap)}>{tocContent}</div>
 			</div>,
 			document.getElementById("main")!
 		)
