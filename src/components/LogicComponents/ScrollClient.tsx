3abccdba6e226d3f67a5dc5f78cd70441e33a1bb
Allenyou <i@allenyou.wang>
2026-07-06 16:33:49 +0800

    fix: navigation bar expand blocked

--- src/components/LogicComponents/ScrollClient.tsx
+++ src/components/LogicComponents/ScrollClient.tsx
@@ -3,25 +3,27 @@
 import { scrollY } from "@/libs/state-management";
 import { throttle } from "@/utils/throttle";
 import { useSetAtom } from "jotai";
-import { useEffect, useEffectEvent } from "react";
+import { useEffectEvent, useLayoutEffect } from "react";
 
 export function ScrollClient() {
-	const setScrollY = useSetAtom(scrollY);
-	const handler = useEffectEvent(
-		throttle(() => {
-			setScrollY(
-				document.body.scrollTop || document.documentElement.scrollTop || 0
-			);
-		}, 100)
-	);
-	useEffect(() => {
-		setScrollY(
-			document.body.scrollTop || document.documentElement.scrollTop || 0
-		);
-		document.addEventListener("scroll", handler, true);
-		return () => {
-			document.removeEventListener("scroll", handler);
-		};
-	}, [setScrollY]);
-	return <></>;
+    const setScrollY = useSetAtom(scrollY);
+    const handler = useEffectEvent(
+        throttle(() => {
+            setScrollY(
+                document.body.scrollTop ||
+                    document.documentElement.scrollTop ||
+                    0,
+            );
+        }, 10),
+    );
+    useLayoutEffect(() => {
+        setScrollY(
+            document.body.scrollTop || document.documentElement.scrollTop || 0,
+        );
+        document.addEventListener("scroll", handler, true);
+        return () => {
+            document.removeEventListener("scroll", handler);
+        };
+    }, [setScrollY]);
+    return <></>;
 }
